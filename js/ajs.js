d7f9392590396128cbe97551e0a75fa7aac403a2
jon4st <45789952+jon4st@users.noreply.github.com>
2019-11-21 16:42:18 +0200

    irasyti uzduoti

---
+++ js/ajs.js
@@ -0,0 +1,25 @@
+function display() {
+    let x = document.getElementById("forma");
+        x.style.display = "block";
+
+
+}
+
+const button = document.querySelector('patvirtinti');
+const vietos = document.querySelector('table');
+
+
+button.addEventListener('click', irasyti)
+
+function irasyti() {
+    const duomenys = document.querySelector('#subject')
+    const vieta = document.createElement('#subjectui');
+
+}
+
+function displayNone() {
+    let y = document.getElementById("forma");
+    y.style.display = "none";
+
+
+}
