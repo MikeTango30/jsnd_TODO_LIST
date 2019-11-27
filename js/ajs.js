bec42afa11b8562a12100adaa80c44af8a2ece00
j.kunciene@gmail.com <48964713+jkunciene@users.noreply.github.com>
2019-11-27 21:17:45 +0200

    neveikia irasymas

--- js/ajs.js
+++ js/ajs.js
@@ -1,28 +1,55 @@
+//-------iskvieciame duomenu ivedimo forma
 function display() {
-    let x = document.getElementById("forma");
-        x.style.display = "block";
-
-
+    let x = document.getElementById("dataform");
+    x.style.display = "block";
 }
 
-const button = document.querySelector('#patvirtinti');
-const addTask = document.querySelector('#subject');
-const tasks = document.querySelector('table');
+//-------duomenu nuskaitymas
+
+const button = document.querySelector('#confirm');
+
+button.addEventListener('click', read)
+
+function read() {
+        let formData = {};
+            formData["Task"] = document.getElementById("TaskInput").value;
+            formData["duedate"] = document.getElementById("duedate").value;
+            formData["inputPriority"] = document.getElementById("inputPriority").value;
+            formData["inputprogres"] = document.getElementById("inputprogres").value;
+            return formData;
 
 
-button.addEventListener('click', irasyti)
-
-function irasyti() {
-    const task = document.createElement('td');
-    task.textContent=addTask.value;
-    tasks.appendChild(task);
-    
+        }
+//------duomenu irasymas i table
+function write(data) {
+    let table = document.getElementById("TasksList").getElementsByTagName('tbody')[0];
+        let newRow = table.insertRow(table.length);
+        cell1 = newRow.insertCell(0);
+        cell1.innerHTML ="<i class=\"far fa-clock\"></i>";
+        cell2 = newRow.insertCell(1);
+        cell2.innerHTML = "<input type=\"checkbox\"> ";
+        cell3 = newRow.insertCell(2);
+        cell3.innerHTML = data.Task;
+        cell4 = newRow.insertCell(3);
+        cell4.innerHTML = data.inputPriority;
+        cell5 = newRow.insertCell(4);
+        cell5.innerHTML = data.duedate;
+        cell6 = newRow.insertCell(5);
+        cell6.innerHTML = data.inputprogres;
+        cell7 = newRow.insertCell(6);
+        cell7.innerHTML = '<a onClick="onDelete(this)">Delete</a>';
 
 }
-
+//-------forma paslepiama supildzius duomenis
 function displayNone() {
-    let y = document.getElementById("forma");
+    let y = document.getElementById("dataform");
     y.style.display = "none";
-
-
 }
+
+    // function onDelete(td) {
+    //     if (confirm('Are you sure to delete this record ?')) {
+    //         row = td.parentElement.parentElement;
+    //         document.getElementById("employeeList").deleteRow(row.rowIndex);
+    //         resetForm();
+    //     }
+    // }
