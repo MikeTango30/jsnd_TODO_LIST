e140ee8ab0f337ea22773fb4fdc2f194fe207142
j.kunciene@gmail.com <48964713+jkunciene@users.noreply.github.com>
2019-11-28 18:45:52 +0200

    nuskaitau i masyva

--- js/ajs.js
+++ js/ajs.js
@@ -4,37 +4,21 @@ function display() {
     x.style.display = "block";
 }
 
-//----mano kintamieji
-
-//1-mygtukas
+//------nuskaitau, irasau i lentele
 
 const button = document.querySelector('#confirm');
-button.addEventListener('click', readWrite);
+button.addEventListener('click', read);
+let duomenuMasyvas = [];
+function read() {
+    duomenuMasyvas.push(document.getElementById("TaskInput").value);
+    duomenuMasyvas.push(document.getElementById("duedate").value);
+    duomenuMasyvas.push(document.getElementById("inputPriority").value);
+    duomenuMasyvas.push(document.getElementById("inputProgres").value);
+    //console.log(duomenuMasyvas);
 
-//---2 - pirmas col su icon
-let stulpelis1 = document.querySelector('#ForIcon');
-const ikona = "<i class=\"material-icons\"></i>";
-
-
-//-------duomenu nuskaitymo, irasymo funkcija
-
-function readWrite() {
-    let vieta = document.getElementById("TasksList").getElementsByTagName('tbody');
-
-    stulpelis1.innerHTML = ikona;
-}
+        }
 
 
-//-------forma paslepiama supildzius duomenis
-function displayNone() {
-    let y = document.getElementById("dataform");
-    y.style.display = "none";
-}
 
-    // function onDelete(td) {
-    //     if (confirm('Are you sure to delete this record ?')) {
-    //         row = td.parentElement.parentElement;
-    //         document.getElementById("employeeList").deleteRow(row.rowIndex);
-    //         resetForm();
-    //     }
-    // }
+
+
